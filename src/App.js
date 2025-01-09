65f782fff7fed1e955f186f8a518a5e192ec1794
Stefanizam <bozinovski.stefan@gmail.com>
2025-01-09 16:59:24 +0100

    Make initial push and organise

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/Main.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container-fluid p-0 m-0">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+
+  return (
+    <nav class="navbar navbar-expand-lg bg-dark text-white w-100 d-flex flex-fill justify-content-between px-5">
+      <strong class="navbar-brand text-white text-uppercase" >Stefan's Lupo Service History</strong>
+        <div class="d-flex">
+          <input class="mx-2 form-control" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar;
