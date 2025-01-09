925a5d2129867f9145700b8f85e2a9f6008e25aa
Stefanizam <bozinovski.stefan@gmail.com>
2025-01-09 20:53:36 +0100

    Improve navbar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App container-fluid p-0 m-0">
-      <header className="App-header">
         <NavBar />
-      </header>
     </div>
   );
 }
