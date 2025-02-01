657796c26d69e65d992213865920736c9bcc2ca9
Stefanizam <bozinovski.stefan@gmail.com>
2025-02-01 21:41:13 +0100

    Push css updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,8 @@ import React from "react";
 const NavBar = () => {
 
   return (
-      <nav className="navbar navbar-expand-lg fixed-top bg-dark text-white w-100 d-flex flex-fill justify-content-between px-5">
-        <strong className="navbar-brand text-white text-uppercase" >Stefan's Lupo Service History</strong>
-        <div className="d-flex">
+      <nav className="navbar navbar-expand-lg fixed-top text-white w-100 d-flex flex-fill justify-content-center px-5">
+        <div className="d-flex w-50">
           <input className="mx-2 form-control" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success" type="submit">Search</button>
         </div>
--- src/components/ServiceHistory.jsx
+++ src/components/ServiceHistory.jsx
@@ -7,9 +7,9 @@ const ServiceHistory = () => {
     const getData = async () => {
       const dataResponse = await fetch("http://127.0.0.1:8000/", { method: "GET", headers: { "Access-Control-Allow-Headers": "*" } });
       const responseJson = await dataResponse.json();
-
       setServiceData(responseJson)
     }
+
     getData();
   }, [])
 
@@ -18,9 +18,18 @@ const ServiceHistory = () => {
       <div style={{ paddingTop: "4rem" }} className="overflow-hidden d-flex justify-content-center w-100">
         <div className="w-50">
           {serviceData && serviceData.map((item, index) => {
-            return (<div className="card border-success bg-dark text-white mb-4 service-card" key={`item-${index}`}>
-              <div className="card-body">
-                {item.shop}
+            return (<div className="card border-success text-white mb-4 service-card bg-tertiary" key={`item-${index}`}>
+              <div className="card-body p-0">
+                <h3 className="card-title mb-1 px-4 pt-4"><strong>{item.shop}</strong></h3>
+                <div className="d-flex flex-wrap">
+                  <span className="card-text px-4 text-success text-nowrap">{item.date}</span>
+                  <span className="card-text px-4 text-success text-nowrap">{item.odometer} km</span>
+                </div>
+                <ul className="list-group list-group-flush pt-4 overflow-hidden">
+                  {item.services.map((service, index) => {
+                    return <div className="service-item" key={`service-${index}`}>{service}</div>
+                  })}
+                </ul>
               </div>
             </div>)
           })}
